5443c4fae5a410e431647a04d217570bbd74f8d3
joao <joaopedroazevedo1980@gmail.com>
2026-05-08 20:00:38 -0300

    Teste

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Digite sua Idade");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade >= 18)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("Você é maior de Idade");
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Você é menor de Idade");
+}
+Console.ResetColor();
